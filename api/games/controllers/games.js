7ef080bab829356a6a9f090f20b95520928649fa
André Ascensão <andremiguelaa@gmail.com>
2021-09-12 01:23:26 +0100

    token validation

--- api/games/controllers/games.js
+++ api/games/controllers/games.js
@@ -1,8 +1,27 @@
-'use strict';
+"use strict";
+
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  update: async (ctx) => {
+    const { id } = ctx.params;
+    let entity = await strapi.services.games.findOne({ id });
+    const token = ctx.request.body.token;
+    let validToken = false;
+    entity.teams.forEach((team) => {
+      if (team.token === token) {
+        validToken = true;
+      }
+    });
+    if (validToken) {
+      entity = await strapi.services.games.update({ id }, ctx.request.body);
+      return sanitizeEntity(entity, { model: strapi.models.games });
+    }
+    return ctx.throw(403, 'invalid_token');
+  },
+};
